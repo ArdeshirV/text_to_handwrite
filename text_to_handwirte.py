a5584c73a50dc9387e5a0172ee407eacdafdf846
nima <nimiologyy@gmail.com>
2021-05-18 14:14:38 +0430

    finished

--- text_to_handwirte.py
+++ text_to_handwirte.py
@@ -1,25 +1,30 @@
 import tkinter
+from tkinter import filedialog, messagebox
 import pywhatkit
 
 def HANDWRITE(TEXT,DIRECTORY):
     print('[HANDWRITE] gettng image...')
     pywhatkit.text_to_handwriting(TEXT,save_to=DIRECTORY)
     print("[HANDWRITE] Done!")
+    messagebox.showinfo('Satus','Done!')
 
 
+def CLICK():
+    DIRECTORYFINDER = filedialog.asksaveasfilename(title='Save as file',
+                                                   filetypes=(('png file', '*.png'), ('all files', '*.*')))
+    TEXT = INPUT.get("1.0",'end')
+    HANDWRITE(TEXT,DIRECTORYFINDER+'.png')
 
 WINDOW = tkinter.Tk()
-WINDOW.configure(bg='light blue')
+WINDOW.configure()
 WINDOW.geometry('800x500')
 WINDOW.title('Text To Handwriting')
 WINDOW.resizable(False,False)
-LABEL = tkinter.Label(WINDOW,text='STATUS : ',bg='light blue',fg='black')
-BUTTON = tkinter.Button(WINDOW,text='CONVERT IT!',height='5',width='30',bg='gray')
-INPUT = tkinter.Text(WINDOW,height='30',width="50",bg='gray',fg='black')
+BUTTON = tkinter.Button(WINDOW,text='CONVERT IT!',height='5',width='30',command=CLICK)
+INPUT = tkinter.Text(WINDOW,height='20',width="96")
 
-LABEL.place(x=580,y=20)
-BUTTON.place(x=500, y=350)
-INPUT.place(x=15,y=7)
+BUTTON.place(x=285, y=370)
+INPUT.place(x=15,y=9)
 
 WINDOW.mainloop()
 
